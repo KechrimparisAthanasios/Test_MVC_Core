2c929691f053760ef2ee80f7cf0df61245004f6f
KechrimparisAthanasios <a.kechrimparis@gmail.com>
2018-08-21 16:28:21 +0200

    First

--- MVC_Core/Program.cs
+++ MVC_Core/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Core_mvc_test
+namespace MVC_Core
 {
     public class Program
     {
--- MVC_Core/Startup.cs
+++ MVC_Core/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Core_mvc_test
+namespace MVC_Core
 {
     public class Startup
     {
